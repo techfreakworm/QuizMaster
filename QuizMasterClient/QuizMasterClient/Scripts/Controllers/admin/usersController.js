22862b8ccb25f3db987989a0a48db43d9a996c2e
Mayank Gupta <techfreakworm@gmail.com>
2018-01-16 21:34:16 +0530

    Add table layout to show user informations

--- QuizMasterClient/QuizMasterClient/Scripts/Controllers/admin/usersController.js
+++ QuizMasterClient/QuizMasterClient/Scripts/Controllers/admin/usersController.js
@@ -1,3 +1,39 @@
-﻿adminPortalApp.controller('usersController', ['$scope', function ($scope) {
-    $scope.message = 'Hello from users Controller';
+﻿adminPortalApp.controller('usersController', ['$scope', '$http', function ($scope, $http) {
+
+    var adminDetails;
+    var presenterDetails;
+
+    //ADMIN FUNCTIONS
+    $scope.editAdminUser = function (index) {
+
+    };
+
+    $scope.deleteAdminUser = function (index) {
+
+    };
+
+    $scope.initAdmin = function () {
+
+        var config = {
+            headers: {
+                'Content-Type':'application/json'
+            }
+        };
+
+        $http.post("")
+    };
+
+
+    //PRESENTER FUNCTIONS
+    $scope.editPresenterUser = function (index) {
+
+    };
+
+    $scope.deletePresenterUser = function (index) {
+
+    };
+
+    $scope.initPresenter = function () {
+
+    };
 }]);
