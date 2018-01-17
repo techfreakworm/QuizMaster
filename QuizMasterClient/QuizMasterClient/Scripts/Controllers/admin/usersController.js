1b6854b293fc0a879554d8d6682c52d41830c90b
Mayank Gupta <techfreakworm@gmail.com>
2018-01-17 12:17:40 +0530

    Complete User view in admin SPA

--- QuizMasterClient/QuizMasterClient/Scripts/Controllers/admin/usersController.js
+++ QuizMasterClient/QuizMasterClient/Scripts/Controllers/admin/usersController.js
@@ -1,18 +1,29 @@
-﻿adminPortalApp.controller('usersController', ['$scope', '$http', function ($scope, $http) {
+﻿adminPortalApp.controller('usersController', ['$scope', '$http', '$cookies', function ($scope, $http, $cookies) {
 
-    var adminDetails;
-    var presenterDetails;
+    /*------------------GLOABLS FOR ALL-----------------------*/
+    $scope.isEditVisible = false;
+    $scope.isAddVisible = false;
+
+
+    /*-------------------------------------------ADMIN--------------------------------------*/
+    //ADMIN GLOBALS
+    $scope.isAdminEditVisible = false;
 
     //ADMIN FUNCTIONS
-    $scope.editAdminUser = function (index) {
+    $scope.addAdminUser = function () { //Unhide add admin menu
+        
+    };
+
+    $scope.editAdminUser = function (index) { //Unhide edit presenter menu
+        $scope.isEditVisible = true;
+        $scope.isAdminEditVisible = true;
+    };
+
+    $scope.deleteAdminUser = function (index) { //delete admin user
 
     };
 
-    $scope.deleteAdminUser = function (index) {
-
-    };
-
-    $scope.initAdmin = function () {
+    $scope.initAdmin = function () { //initialize admin table
 
         var config = {
             headers: {
@@ -20,20 +31,63 @@
             }
         };
 
-        $http.post("")
+        $http.post("http://localhost:50827/api/user/getadmin", $cookies.getObject('user'), config).then(function (successResponse) {
+            $scope.adminUsers = successResponse.data;
+            //console.log(adminDetails);
+        }, function (errorResponse) {
+            console.log("Cannot fetch all admin user details");
+        });
     };
 
+    $scope.saveAdminEdit = function () {//Edit an admin user
+
+    };
+
+    $scope.saveAdminAdd = function () {//Add an admin user
+
+    };
+
+
+
+    /*--------------------------------------PRESENTER----------------------------------------*/
+    //PRESENTER GLOBALS
+    $scope.isPresenterEditVisible = false;
 
     //PRESENTER FUNCTIONS
-    $scope.editPresenterUser = function (index) {
+    $scope.addPresenterUser = function () {//Unhide add presenter menu
 
     };
 
-    $scope.deletePresenterUser = function (index) {
+    $scope.editPresenterUser = function (index) {  //Unhide edit presenter menu
 
     };
 
-    $scope.initPresenter = function () {
+    $scope.deletePresenterUser = function (index) { //Delete a presenter
 
     };
+
+    $scope.initPresenter = function () { //Initialize presenter table
+        var config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        $http.post("http://localhost:50827/api/user/getpresenter", $cookies.getObject('user'), config).then(function (successResponse) {
+            $scope.presenterUsers = successResponse.data;
+            console.log($scope.presenterUsers);
+        }, function (errorResponse) {
+            console.log("Cannot fetch all presenter user details");
+        });
+    };
+
+    $scope.savePresenterEdit = function () { //Save presenter edits
+
+    };
+
+    $scope.savePresenterAdd = function () { //Add a presenter user
+
+    };
+
+    
 }]);
