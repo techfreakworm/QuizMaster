e02ab617d6946b19c253f4fc8a435559643f9c6e
Mayank Gupta <techfreakworm@gmail.com>
2018-01-18 10:06:48 +0530

    Add Client->presenter->Teams, //TODO:Styling

--- QuizMasterClient/QuizMasterClient/Scripts/Controllers/presenter/teamsController.js
+++ QuizMasterClient/QuizMasterClient/Scripts/Controllers/presenter/teamsController.js
@@ -1,3 +1,17 @@
-﻿presenterApp.controller('teamsController', ['$scope', function ($scope) {
-    $scope.message = 'Hello from teams Controller';
+﻿presenterApp.controller('teamsController', ['$scope', '$http', '$cookies', function ($scope, $http, $cookies)  {
+
+    $scope.initTeam = function () { //initialize admin table
+        var config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        $http.post("http://localhost:50827/api/team/get", $cookies.getObject('user'), config).then(function (successResponse) {
+            $scope.teams = successResponse.data;
+        }, function (errorResponse) {
+            console.log("Cannot fetch all admin user details");
+        });
+    };
+
 }]);
